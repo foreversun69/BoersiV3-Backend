441764a7964d6d82c01da65e8592ab19164fb7a9
unknown <blackcow33ps@gmx.de>
2021-01-05 19:28:50 +0100

    1. commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0a292b256b687172920c8e6efdf8121d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b56034c-7074-4caa-b0a7-40f495636925'
+};
